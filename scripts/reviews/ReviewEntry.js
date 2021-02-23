e2b998625d01a9965680617d40d96ed93d96d4f9
bpotts11 <brittneympotts@gmail.com>
2021-02-22 23:37:47 -0600

    Refactored the delete button in delete entry so that only the user that created the review can delete it

--- scripts/reviews/ReviewEntry.js
+++ scripts/reviews/ReviewEntry.js
@@ -1,12 +1,19 @@
 import { deleteReviews } from "./ReviewDataProvider.js"
+import { authHelper } from "../auth/authHelper.js"
+
+let currentCustomerId = sessionStorage.getItem("soj-customer-id")
 
 // This is HTML format for how the review will render to the dom
 export const ReviewEntryComponent = (review) => {
+    let deleteButton = ""
+    if (review.customerId === parseInt(currentCustomerId)) {
+        deleteButton = `<button class="review__modal--delete" id="deleteButton--${review.id}">Delete</button>`
+    }
     return ` 
         <section id="review--${review.id}" class="reviewEntry">
-            <div class="reviewText">Review: ${review.review}</div>
+            <div class="reviewText">${review.review}</div>
             <div class="reviewRating">${review.rating.label}</div>
-            <button class="review__modal--delete" id="deleteButton--${review.id}">Delete</button>
+            ${deleteButton}
         </section>
             `
 }
